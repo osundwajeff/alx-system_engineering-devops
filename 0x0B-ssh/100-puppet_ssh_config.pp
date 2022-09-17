e2808e48d7d28b5d407781c61338c372be16ad04
osundwajeff <mulamajeff@tuta.io>
2022-09-17 03:51:53 -0700

    fix client config file (puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,6 @@
 # client configuration using puppet
 exec { 'Turn off passwd auth':
-  command => 'bash -c "echo PasswordAuthentification no >> /etc/ssh/ssh_config"',
+  command => 'bash -c "echo PasswordAuthentication no >> /etc/ssh/ssh_config"',
   path    => 'usr/bin:/usr/sbin:/bin'
 }
 exec { 'Declare identity file':
@@ -8,6 +8,6 @@ exec { 'Declare identity file':
   path    => '/usr/bin:/usr/sbin:/bin'
 }
 exec { 'Turn on pubkey auth':
-  command => 'bash -c "echo PubkeyAuthentification yes >> /etc/ssh/ssh_config"',
+  command => 'bash -c "echo PubkeyAuthentication yes >> /etc/ssh/ssh_config"',
   path    => '/usr/bin:/usr/sbin:/bin'
 }
