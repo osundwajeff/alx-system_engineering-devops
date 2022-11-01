1d258f0ba6bec9cd3b3e73319c920400f34e6c43
osundwajeff <mulamajeff@tuta.io>
2022-11-01 08:05:18 -0700

    top ten posts

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""function for Reddit API."""
+import requests
+
+
+def top_ten(subreddit):
+    """Gets top 10 posts from a given subredit"""
+    header = {"User-Agent": "Holberton"}
+    url = "https://www.reddit.com/r/" + subreddit + "/hot.json?limit=10"
+    res = requests.get(url, headers=header)
+    if res.status_code == 200:
+        for i in res.json().get("data", None).get("children", None):
+            print (i.get("data", None).get("title", None))
+    else:
+        return None
+
+
+if __name__ == "__main__":
+    pass
